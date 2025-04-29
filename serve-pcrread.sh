a7d4a41bf8f9e86346c55d06e8eb62c97e8f3837
Gauthier Jolly <gauthier.jolly@canonical.com>
2025-04-29 10:27:37 +0200

    add endpoint for pcr13

--- serve-pcrread.sh
+++ serve-pcrread.sh
@@ -4,6 +4,7 @@ report_dir="$SNAP_COMMON/report"
 mkdir -p "$report_dir"
 PORT="${LISTENING_PORT:-80}"
 
-tpm2_pcrread > "$report_dir/pcr"
+tpm2_pcrread sha256 > "$report_dir/pcr"
+tpm2_pcrread sha256:13 > "$report_dir/pcr13"
 
 python3 -m http.server -d "$report_dir" $PORT
